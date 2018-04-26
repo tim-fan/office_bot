13b423465afeea4d7e2644fe223ca3d59c3e8dc4
Tim Fanselow <tim.fanselow@crown.com>
2018-04-26 14:25:58 +1200

    added amcl localisation with cartographer odom

--- office_bot/cfg/office_bot.lua
+++ office_bot/cfg/office_bot.lua
@@ -1,16 +1,8 @@
--- Copyright 2016 The Cartographer Authors
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---      http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+-- Cartographer config file for office robot.
+-- Compatible with:
+--   cartographer@b1dcb0c
+--   cartographer_ros@f818bc7
+
 
 include "map_builder.lua"
 include "trajectory_builder.lua"
