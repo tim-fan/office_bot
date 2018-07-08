9e5e7517cf72fc25423ff03985d7731348dab7b6
Ubuntu User <tim.fanselow@gmail.com>
2018-07-08 04:14:53 +0000

    Restructure launch files

--- office_bot/scripts/install_start_scripts.sh
+++ office_bot/scripts/install_start_scripts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rosrun robot_upstart install --job office_bot_ros office_bot/launch/office_bot_navigation.launch
+rosrun robot_upstart install --job office_bot_ros office_bot/launch/office_bot.launch
